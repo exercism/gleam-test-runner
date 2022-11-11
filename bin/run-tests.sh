037062ca3a1d0d1075107df2345dcfa6e746a011
Louis Pilfold <louis@lpil.uk>
2022-11-11 12:36:21 +0000

    Appease shellcheck

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -15,28 +15,28 @@ exit_code=0
 
 # Iterate over all test directories
 for test_dir in tests/*; do
-    test_dir_name=$(basename "${test_dir}")
-    test_dir_path=$(realpath "${test_dir}")
-    results_file_path="${test_dir_path}/results.json"
-    expected_results_file_path="${test_dir_path}/expected_results.json"
+  test_dir_name=$(basename "${test_dir}")
+  test_dir_path=$(realpath "${test_dir}")
+  results_file_path="${test_dir_path}/results.json"
+  expected_results_file_path="${test_dir_path}/expected_results.json"
 
-    bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
+  bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
 
-    # OPTIONAL: Normalize the results file
-    # If the results.json file contains information that changes between 
-    # different test runs (e.g. timing information or paths), you should normalize
-    # the results file to allow the diff comparison below to work as expected
-    # sed -i -E \
-    #   -e 's/Elapsed time: [0-9]+\.[0-9]+ seconds//g' \
-    #   -e "s~${test_dir_path}~/solution~g" \
-    #   "${results_file_path}"
+  # OPTIONAL: Normalize the results file
+  # If the results.json file contains information that changes between 
+  # different test runs (e.g. timing information or paths), you should normalize
+  # the results file to allow the diff comparison below to work as expected
+  # sed -i -E \
+  #   -e 's/Elapsed time: [0-9]+\.[0-9]+ seconds//g' \
+  #   -e "s~${test_dir_path}~/solution~g" \
+  #   "${results_file_path}"
 
-    echo "${test_dir_name}: comparing results.json to expected_results.json"
-    diff "${results_file_path}" "${expected_results_file_path}"
+  echo "${test_dir_name}: comparing results.json to expected_results.json"
 
-    if [ $? -ne 0 ]; then
-        exit_code=1
-    fi
+  if ! diff "${results_file_path}" "${expected_results_file_path}"
+  then
+    exit_code=1
+  fi
 done
 
 exit ${exit_code}
