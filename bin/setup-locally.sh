310edb5d49527d10d2cb64eaf0dac33ac88e00eb
Louis Pilfold <louis@lpil.uk>
2022-11-15 11:14:13 +0000

    Precompile deps

--- bin/setup-locally.sh
+++ bin/setup-locally.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Synopsis:
-# Setup the test runner locally by pre-downloading dependency packages.
+# Setup the test runner locally by pre-downloading and compiling dependency
+# packages.
 
 # Example:
 # ./bin/setup-locally.sh
@@ -10,4 +11,4 @@ set -eu
 
 root_dir=$(dirname "$(dirname "$(realpath "$0")")")
 cd "$root_dir"/packages
-gleam deps download
+gleam build
